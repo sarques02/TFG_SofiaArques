c8652845a0653d3d4afcb2b9493789be6132579b
Sofia Arques <126562939+sarques02@users.noreply.github.com>
2024-06-14 20:12:01 +0200

    Update create_txt.py

--- smaller_test/create_txt.py
+++ smaller_test/create_txt.py
@@ -1,29 +1,20 @@
 import os
 import random
 
-# Ruta a la carpeta que contiene las imágenes
-ruta_carpeta_imagenes = '/home/gatv-matteo/Desktop/tfg_sofia/TransWeather_Quantization/data/smaller_test/'
+# Path to the folder containing the images
+images_folder_path = './smaller_test/'
 
-# Nombre del archivo de texto
-nombre_archivo_txt = './allweather.txt'
+# Name of the text file
+txt_file_name = './allweather.txt'
 
-# Obtener nombres de archivos de la carpeta de imágenes (en la carpeta gt)
-ruta_carpeta_gt = os.path.join(ruta_carpeta_imagenes, 'input')
-archivos_imagenes = os.listdir(ruta_carpeta_gt)
+gt_folder_path = os.path.join(images_folder_path, 'input')
+image_files = os.listdir(gt_folder_path)
+valid_extensions = ('.jpg', '.png', '.jpeg')
+image_files = [f for f in image_files if f.lower().endswith(valid_extensions)]
 
-# Filtrar por extensiones de imagen válidas (ajusta según tus extensiones)
-extensiones_validas = ('.jpg', '.png', '.jpeg')
-archivos_imagenes = [f for f in archivos_imagenes if f.lower().endswith(extensiones_validas)]
+with open(txt_file_name, 'w') as txt_file:
+    for image_name in image_files:
+        txt_file.write(os.path.join('./smaller_test/input/', image_name) + '\n')  
 
-# Seleccionar aleatoriamente 50 imágenes
-# archivos_aleatorios = random.sample(archivos_imagenes, 50)
+print(f'The file "{txt_file_name}" has been created')
 
-# Crear o sobrescribir el archivo de texto con los nombres de archivos
-with open(nombre_archivo_txt, 'w') as archivo_txt:
-    for nombre_imagen in archivos_imagenes:
-        archivo_txt.write(os.path.join('/data/smaller_test/input/', nombre_imagen) + '\n')  # Agregar etiqueta 0
-
-print(f'Se ha creado el archivo "{nombre_archivo_txt}"')
-
-# Luego puedes usar este archivo en la función proporcionada
-# Si image_list es None, se utilizará el archivo recién creado
